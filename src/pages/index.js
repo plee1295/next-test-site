e79ee674fd9bdad58e5ca12345558566a4f7456c
Parker Lee <parker.lee.1295@gmail.com>
2022-02-08 13:02:03 -0600

    Do the needful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-export default function Index() {
-  return <h1>BOOP</h1>;
+import Head from 'next/head'
+
+export default function Index () {
+  return (
+    <div>
+      <Head>
+        <script async defer src="https://storage.googleapis.com/lucky-orange-staging-public/core/lo.js?site-id=a461c599"></script>
+      </Head>
+      <h1>BOOP</h1>
+    </div>
+  )
 }
